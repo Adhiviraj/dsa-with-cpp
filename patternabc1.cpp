7635b8c69a4a45bda7eb2a52d1d2fadd43a477b8
Adhiviraj <paladhiviraj@gmail.com>
2025-03-26 00:25:46 +0530

    all in

---
+++ patternabc1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int row=1;
+    
+    while(row<=n)
+    {
+        int column = 1;
+            while(column<=n){
+                char ch = 'A'+ row +column - 2;
+                cout<<ch ;
+                column++;
+               
+            }
+            row++;
+            cout<<endl;
+    }
+}
