7c4b6e1c789b0f72edc63d0506d6ee1f23ca8205
Adhiviraj <paladhiviraj@gmail.com>
2025-09-05 16:49:02 +0530

    05'09'25

---
+++ TLEarrduplicates.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int i=0,j=0;
+    vector<int> ans;
+    
+    int n,m;
+    cout<<"Enter the size of two array-";
+    cin>>n>>m;
+    for()
+    while(i<n&j<m)
+}
+
---
+++ sort01.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namepsace std;
+
+
+
+int main(){
+    int arr[8]={0,1,0,0,1,1,0,1}
+
+    sortOne(arr,8);
+    printarray(arr,8);
+}
