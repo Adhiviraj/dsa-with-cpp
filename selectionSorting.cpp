d34e5eb7a9698360aec67b1e073192ce48f155c7
Adhiviraj <paladhiviraj@gmail.com>
2025-10-18 01:27:29 +0530

    18th oct'25

---
+++ selectionSorting.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+   vector<int> selectionSorting(vector<int> arr,int n){
+    
+    for(int i=0;i<n-1;i++){
+        int minIndex = i;
+
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[minIndex]){
+                minIndex=j;
+            } 
+        }
+        swap(arr[minIndex],arr[i]);
+    }
+
+    return arr;
+
+}
+
+int main(){
+    
+    vector<int> arr={5,3,6,8,9,2};
+    int n=arr.size();
+    vector<int> SortedArr = selectionSorting(arr,n);
+    cout<<"{";
+    for(int i=0; i<n;i++){
+        cout<<SortedArr[i];
+        if(i!=n-1) cout<<",";
+    }
+    cout<<"}"<<endl;
+
+}
+
