68c09f3e03dd8d0097a9932111aab31728100622
Adhiviraj <paladhiviraj@gmail.com>
2026-09-21 20:16:57 +0530

    merge to sort

--- LinkedList/sortingAlgorithms/mergeSort.cpp
+++ LinkedList/sortingAlgorithms/mergeSort.cpp
@@ -25,17 +25,81 @@ public:
 
 };
 
-void InsertAtTail(Node* tail,int n){
+void InsertAtTail(Node*& tail,int n){
     Node* temp = new Node(n);
 
     tail->next = temp;
 
-    tail = temp;
+    tail = tail->next;
 }
 
+Node* Merge2Sort(Node* left, Node* right){
+    int val = 0;
+    if(left->data <= right->data){
+        val = left->data;
+        left = left->next;
+    }else{
+        val = right->data;
+        right = right->next;
+    }
+    Node* temp = new Node(val);
+    Node* head = temp;
+    while(left != NULL && right != NULL){
+        val = 0;
+        if(left->data <= right->data){
+            val = left->data;
+            left = left->next;
+        }else{
+            val = right->data;
+            right = right->next;
+        }
+        Node* temp1 = new Node(val);
+        temp->next = temp1;
+        temp = temp->next;
+    }
 
-void ListSorting(Node* head){
-    
+    if(left != NULL){
+        temp->next = left;
+    }else if(right != NULL){
+        temp->next = right;
+    }
+
+    return head;
+}
+
+Node* middle(Node* head){
+    int cnt = 0;
+    Node* temp = head;
+    while(temp != NULL){
+        cnt++;
+        temp = temp->next;
+    }
+
+    int mid = cnt/2;
+    cnt = 0;
+    while(head != NULL){
+        cnt++;
+        if(cnt == mid){
+            return head;
+        }
+        head = head->next;
+    }
+}
+
+Node* ListSorting(Node* head){
+    if(head == NULL || head->next == NULL){
+        return head;
+    }
+
+    Node* MiddleNode = middle(head);
+
+    Node* LeftNode = head;
+    Node* RightNode = MiddleNode->next;
+    MiddleNode->next = NULL;
+    LeftNode = ListSorting(LeftNode);
+    RightNode = ListSorting(RightNode);
+
+    return Merge2Sort(LeftNode,RightNode);
 }
 
 
@@ -62,6 +126,10 @@ int main(){
         InsertAtTail(tail,n);
     }
 
-    ListSorting(n1);
-    
+    Node* head = ListSorting(n1);
+
+    while(head != NULL){
+        cout << "Node value is - " << head->data << endl;
+        head = head->next;
+    }
 }
